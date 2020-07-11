5e1d5e9a6d400992f406009767c30d403a62c31b
NVcoder24 <60108915+NVcoder24@users.noreply.github.com>
2020-07-11 18:25:06 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,27 +11,27 @@ if [ $numb = "1" ]
 then
 	pkg install python
 	pkg install dos2unix
-	cp ~/Spymer_Lite_Edition/main.py /data/data/com.termux/files/usr/bin/spymer2
-	dos2unix /data/data/com.termux/files/usr/bin/spymer2
-	chmod -R 777 ~/spymer2
-	chmod 777 /data/data/com.termux/files/usr/bin/spymer2
+	cp ~/Spymer_Lite_Edition/main.py /data/data/com.termux/files/usr/bin/spymer
+	dos2unix /data/data/com.termux/files/usr/bin/spymer
+	chmod -R 777 ~/spymer
+	chmod 777 /data/data/com.termux/files/usr/bin/spymer
 	clear
-	echo "Готово! (Чтобы запустить впишите spymer2)"
+	echo "Готово! (Чтобы запустить впишите spymer)"
 else
 	if [ $numb = "2" ]
 	then
 		if [ "$(whoami)" != 'root' ];
 		then
-			echo "У вас нет прав. Запустите install.sh с root правами (sudo sh ~/spymer/install.sh)"
+			echo "У вас нет прав. Запустите install.sh с root правами (sudo sh ~/Spymer_Lite_Edition/install.sh)"
 			exit
 		else
 			apt install python3 python3-pip dos2unix
-			cp ~/Spymer_Lite_Edition/main.py /usr/bin/spymer2
-			dos2unix /usr/bin/spymer2
-			chmod 777 /usr/bin/spymer2
-			chmod -R 777 ~/spymer2
+			cp ~/Spymer_Lite_Edition/main.py /usr/bin/spymer
+			dos2unix /usr/bin/spymer
+			chmod 777 /usr/bin/spymer
+			chmod -R 777 ~/spymer
 			clear
-			echo "Готово! (Чтобы запустить впишите spymer2)"
+			echo "Готово! (Чтобы запустить впишите spymer)"
 		fi
 	else
 		if [ $numb = "3" ] 
@@ -39,11 +39,11 @@ else
 			apk add python
 			apk add python3
 			apk add dos2unix
-			cp ~/Spymer_Lite_Edition/main.py /usr/bin/spymer2
-			dos2unix /usr/bin/spymer2
-			chmod 777 /usr/bin/spymer2
+			cp ~/Spymer_Lite_Edition/main.py /usr/bin/spymer
+			dos2unix /usr/bin/spymer
+			chmod 777 /usr/bin/spymer
 			clear
-			echo "Готово! (Чтобы запустить впишите spymer2)"
+			echo "Готово! (Чтобы запустить впишите spymer)"
 		else
 			clear
 			echo "Некорректный ввод"
