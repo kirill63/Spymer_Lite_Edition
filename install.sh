f49cf5e22f148d3a79bb24f1baba10615a96cee3
NVcoder24 <60108915+NVcoder24@users.noreply.github.com>
2020-07-11 18:09:30 +0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,47 @@
+clear
+echo "Spymer Lite Edition installer"
+echo "Кто ты ?"
+echo ""
+echo "1) Termux"
+echo "2) Другой Unix"
+echo "3) iSH"
+read numb
+if [ $numb = "1" ]
+then
+	pkg install python
+	pkg install dos2unix
+	cp ~/Spymer_Lite_Edition-master/main.py /data/data/com.termux/files/usr/bin/spymer2
+	dos2unix /data/data/com.termux/files/usr/bin/spymer2
+	chmod -R 777 ~/spymer2
+	chmod 777 /data/data/com.termux/files/usr/bin/spymer2
+	echo "Готово! (Чтобы запустить впишите spymer2)"
+else
+	if [ $numb = "2" ]
+	then
+		if [ "$(whoami)" != 'root' ];
+		then
+			echo "У вас нет прав. Запустите install.sh с root правами (sudo sh ~/spymer/install.sh)"
+			exit
+		else
+			apt install python3 python3-pip dos2unix
+			cp ~/Spymer_Lite_Edition-master/main.py /usr/bin/spymer2
+			dos2unix /usr/bin/spymer2
+			chmod 777 /usr/bin/spymer2
+			chmod -R 777 ~/spymer2
+			echo "Готово! (Чтобы запустить впишите spymer2)"
+		fi
+	else
+		if [ $numb = "3" ] 
+		then
+			apk add python
+			apk add python3
+			apk add dos2unix
+			cp ~/Spymer_Lite_Edition-master/main.py /usr/bin/spymer2
+			dos2unix /usr/bin/spymer2
+			chmod 777 /usr/bin/spymer2
+			echo "Готово! (Чтобы запустить впишите spymer2)"
+		else
+			echo "Некорректный ввод"
+		fi
+	fi
+fi
